e94211331f1898557a1801e52185e54a82a6ec02
Hkc-1673 <habibkc71@gmail.com>
2020-10-25 21:24:08 +0200

    Modified components

--- ReactNativeProject/src/pages/RestaurantList.js
+++ ReactNativeProject/src/pages/RestaurantList.js
@@ -1,14 +1,70 @@
-import React from "react";
-import { SafeAreaView, View, Text} from "react-native";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View, Text, FlatList } from 'react-native';
+import { RestaurantItem, SearchBar } from '../components';
+
+let originalList = [];
 
 const RestaurantList = (props) => {
+    const [restaurantList, setRestaurantList] = useState([]);
+    const { selectedCity } = props.route.params;
+
+
+    const fetchRestaurants = () => {
+        axios.get(
+            'http://opentable.herokuapp.com/api/restaurants',
+            {
+                params: {
+                    city: selectedCity
+                }
+            })
+            .then(response => {
+                setRestaurantList(response.data.restaurants);
+                originalList = [...response.data.restaurants];
+            })
+    }
+
+    useEffect(() => {
+        fetchRestaurants();
+    }, [])
+
+    const renderRestaurants = ({ item }) => {
+        return (
+            <RestaurantItem
+                restaurant={item}
+            />
+        )
+    }
+
+    function searchRestaurant(search) {
+        const filteredRestaurants = originalList.filter(restaurant => {
+            const text = search.toUpperCase();
+            const restaurantName = restaurant.name.toUpperCase();
+
+            return restaurantName.indexOf(text) > -1;
+        })
+
+        setRestaurantList(filteredRestaurants);
+    }
+
     return (
         <SafeAreaView>
             <View>
-                <Text>RestaurantList</Text>
+                <View>
+                    <Text style={{ margin: 5, fontWeight: 'bold', fontSize: 30 }}>{selectedCity} Restaurants</Text>
+                    <SearchBar
+                        placeholder="Search a restaurant..."
+                        onSearch={(value) => searchRestaurant(value)}
+                    />
+                </View>
+                <FlatList
+                    keyExtractor={(_, index) => index.toString()}
+                    data={restaurantList}
+                    renderItem={renderRestaurants}
+                />
             </View>
         </SafeAreaView>
     )
 }
 
-export {RestaurantList};
+export { RestaurantList }
