3d9bd6bdeaee14e016d72868a5a6ddfaa8952d79
Hkc-1673 <habibkc71@gmail.com>
2020-10-25 18:21:53 +0200

    created files

---
+++ ReactNativeProject/src/Router.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { CityList, RestaurantList, RestaurantDetail } from './pages/index';
+
+const Stack = createStackNavigator();
+
+
+const Router = () => {
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Cities" component={CityList}/>
+                <Stack.Screen name="Restaurants" component={RestaurantList}/>
+                <Stack.Screen name="Details" component={RestaurantDetail}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+
+    )
+}
+
+export default Router;
